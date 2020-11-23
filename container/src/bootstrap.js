10a33f53724bea77c38cbb790a8bae0372d9ba74
Agustin Carnevale <acarnevale@ecointeractive.com>
2020-11-22 23:11:54 -0300

    initial commit

---
+++ container/src/bootstrap.js
@@ -0,0 +1,7 @@
+import { mount } from 'products/ProductsIndex';
+import { mount as cartMount } from 'cart/CartShow';
+
+console.log('Container!!');
+
+mount(document.querySelector('#my-products'));
+cartMount(document.querySelector('#my-cart'))
